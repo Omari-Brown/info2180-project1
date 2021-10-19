6917398ace87790a63c2c3cf06801de8f9fe5bc4
Omari Brown <Omaribrown16@gmail.com>
2021-10-19 01:15:35 -0500

    Added Javascript to newsletter form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 /* Add your JavaScript to this file */
+window.onload = function ()
+{
+    console.log("Page loaded.");
+    const mybutton = document.getElementsByClassName("btn")[1];
+    console.log(mybutton);
+    const messagebox = document.getElementsByClassName("message")[0];
+    console.log(messagebox);
+    mybutton.addEventListener("click", function (event)
+    {
+        event.preventDefault();
+        console.log("Nope");
+        const email = document.getElementById("email").value;
+        console.log(email);
+        if (email.length==0)
+        {
+            console.log("Invalid email length");
+            messagebox.innerText = "Please enter a valid email address.";
+        } else
+        {
+            console.log("Valid email length");
+            messagebox.innerText = "Thank you! Your email address<" + email + "> has been added to our mailing list!";
+        } 
+    });
+}
